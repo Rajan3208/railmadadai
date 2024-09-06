862cffb0c3272ed3a151e1c7b25a4e3340dafd5d
Rajan Singh <145154389+Rajan3208@users.noreply.github.com>
2024-09-06 19:37:09 +0530

    Update railmadadai.py

--- railmadadai.py
+++ railmadadai.py
@@ -123,6 +123,19 @@ st.markdown("""
     }
 </style>
 """, unsafe_allow_html=True)
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/66db0916ea492f34bc0e8171/1i73pn0eb';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 
 # Layout
 col1, col2, col3 = st.columns([2,5,2])
